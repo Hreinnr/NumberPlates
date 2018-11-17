76d759148fc620d0b54dc36726bb3acb3dd4c40c
Hreinn Rúnarsson <hreinnrunarsson@Hreinns-MacBook-Pro.local>
2018-11-17 14:50:56 +0000

    Show data from api

--- index.js
+++ index.js
@@ -11,5 +11,25 @@ function getInfo(numer){
 }
 
 function showInfo(data){
-  console.log(data);
+  console.log(data.results[0]);
+  var type = data.results[0].type;
+  var subType = data.results[0].subType;
+  var color = data.results[0].color;
+  var weight = data.results[0].weight;
+  var status = data.results[0].status;
+  var registryNumber = data.results[0].registryNumber;
+  var registeredAt = data.results[0].registeredAt;
+  var pollution = data.results[0].pollution;
+  var nextCheck = data.results[0].nextCheck;
+  var factoryNumber = data.results[0].factoryNumber;
+  document.getElementById("type").innerHTML = type;
+  document.getElementById("subType").innerHTML = subType;
+  document.getElementById("color").innerHTML = color;
+  document.getElementById("weight").innerHTML = weight;
+  document.getElementById("status").innerHTML = status;
+  document.getElementById("registryNumber").innerHTML = registryNumber;
+  document.getElementById("registeredAt").innerHTML = registeredAt;
+  document.getElementById("pollution").innerHTML = pollution;
+  document.getElementById("nextCheck").innerHTML = nextCheck;
+  document.getElementById("factoryNumber").innerHTML = factoryNumber;
 }
